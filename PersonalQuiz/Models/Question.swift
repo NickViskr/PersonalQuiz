7338ae21420af07eff2bafdfef63acd52ed2ec87
NickViskr <nviskr@gmail.com>
2022-07-22 00:32:31 +0500

    Model created questions loaded

--- PersonalQuiz/Models/Question.swift
+++ PersonalQuiz/Models/Question.swift
@@ -9,6 +9,41 @@ struct Question {
     let title: String
     let responseType: ResponseType
     let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу Вы предпочитаете?",
+                responseType: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle),
+                ]
+        ),
+            Question(
+                title: "Что Вам нравиться больше?",
+                responseType: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle),
+                ]
+        ),
+            Question(
+                title: "Любите ли Вы поездки на машине?",
+                responseType: .ranger,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Не очень", animal: .turtle),
+                    Answer(title: "Люблю", animal: .rabbit),
+                    Answer(title: "Обожаю", animal: .dog),
+                ]
+        )
+        ]
+    }
 }
 
 enum ResponseType {
@@ -27,6 +62,19 @@ enum Animal: Character {
     case cat = "😺"
     case rabbit = "🐰"
     case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вы собака и лаете на прохожих"
+        case .cat:
+            return "Вы кошка и мурлыкаете себе в усы"
+        case .rabbit:
+            return "Вы кролик с длинными ушами"
+        case .turtle:
+            return "Вы мудрая черепаха"
+        }
+    }
 }
 
 
